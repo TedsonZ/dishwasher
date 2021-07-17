70330998e051a200124bbf6d1be4cab6084f015a
Ayouloki <60135791+Ayouloki@users.noreply.github.com>
2021-07-17 15:17:06 -0300

    Update dishwasher.ino

--- dishwasher.ino
+++ dishwasher.ino
@@ -1,4 +1,4 @@
-/*********
+/*
   Rui Santos
   Complete project details
    - Arduino IDE: https://RandomNerdTutorials.com/esp32-ota-over-the-air-arduino/
@@ -9,45 +9,83 @@
   
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
-*********/
+*/
 
 // Import required libraries
 #include <Arduino.h>
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
+#include <Arduino_JSON.h>
 #include <AsyncElegantOTA.h>
-#include "pag.h"
 
 // Replace with your network credentials
 const char* ssid = "VIA";
 const char* password = "123456789";
 
-bool ledState = 0;
-const int ledPin = 2;
-
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+
+// Create a WebSocket object
 AsyncWebSocket ws("/ws");
 
+// Set number of outputs
+#define NUM_OUTPUTS  4
 
+// Assign each GPIO to an output
+int outputGPIOs[NUM_OUTPUTS] = {2, 4, 12, 14};
 
-void notifyClients() {
-  ws.textAll(String(ledState));
+// Initialize SPIFFS
+void initSPIFFS() {
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An error has occurred while mounting SPIFFS");
+  }
+  Serial.println("SPIFFS mounted successfully");
+}
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+String getOutputStates(){
+  JSONVar myArray;
+  for (int i =0; i<NUM_OUTPUTS; i++){
+    myArray["gpios"][i]["output"] = String(outputGPIOs[i]);
+    myArray["gpios"][i]["state"] = String(digitalRead(outputGPIOs[i]));
+  }
+  String jsonString = JSON.stringify(myArray);
+  return jsonString;
+}
+
+void notifyClients(String state) {
+  ws.textAll(state);
 }
 
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo*)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
     data[len] = 0;
-    if (strcmp((char*)data, "toggle") == 0) {
-      ledState = !ledState;
-      notifyClients();
+    if (strcmp((char*)data, "states") == 0) {
+      notifyClients(getOutputStates());
+    }
+    else{
+      int gpio = atoi((char*)data);
+      digitalWrite(gpio, !digitalRead(gpio));
+      notifyClients(getOutputStates());
     }
   }
 }
 
-void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,AwsEventType type,
              void *arg, uint8_t *data, size_t len) {
   switch (type) {
     case WS_EVT_CONNECT:
@@ -66,49 +104,32 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType 
 }
 
 void initWebSocket() {
-  ws.onEvent(onEvent);
-  server.addHandler(&ws);
-}
-
-String processor(const String& var){
-  Serial.println(var);
-  if(var == "STATE"){
-    if (ledState){
-      return "ON";
-    }
-    else{
-      return "OFF";
-    }
-  }
-  return String();
+    ws.onEvent(onEvent);
+    server.addHandler(&ws);
 }
 
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
-  
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
+  // Set GPIOs as outputs
+  for (int i =0; i<NUM_OUTPUTS; i++){
+    pinMode(outputGPIOs[i], OUTPUT);
   }
-
-  // Print ESP Local IP Address
-  Serial.println(WiFi.localIP());
-
+  initSPIFFS();
+  initWiFi();
   initWebSocket();
 
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
+    request->send(SPIFFS, "/index.html", "text/html",false);
   });
+
+  server.serveStatic("/", SPIFFS, "/");
 
   // Start ElegantOTA
   AsyncElegantOTA.begin(&server);
+  
   // Start server
   server.begin();
 }
@@ -116,5 +137,4 @@ void setup(){
 void loop() {
   AsyncElegantOTA.loop();
   ws.cleanupClients();
-  digitalWrite(ledPin, ledState);
 }
